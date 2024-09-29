b650f85f479b563dae414fc244d155a8a9d02c49
jaredgoddard <jaredbgoddard@gmail.com>
2024-09-29 14:45:36 -0400

    Display web view in sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { HelloWorldTreeDataProvider } from './todo-view';
+import { MyWebviewViewProvider } from './task-view';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "stars-todo-list" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('stars-todo-list.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	const disposable = vscode.commands.registerCommand(
+    'stars-todo-list.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from Star&#39;s Todo List!');
 	});
 
-  const helloWorldTreeDataProvider = new HelloWorldTreeDataProvider();
-  vscode.window.registerTreeDataProvider('stars-todo-list', helloWorldTreeDataProvider);
+  const webview = vscode.window.registerWebviewViewProvider(
+    'stars-todo-list.task-list-view', 
+    new MyWebviewViewProvider(context)
+  );
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(webview);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
