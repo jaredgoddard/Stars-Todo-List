05033dfcc6bfe58b68b470db08ac959e40358e48
roomee1-admin <jaredbgoddard@gmail.com>
2024-10-02 21:12:06 -0400

    install typestyle

--- src/views/task-list/components/task/checkbox.tsx
+++ src/views/task-list/components/task/checkbox.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './checkbox.module.css';
+import { style  } from 'typestyle';
+import * as vscode from 'vscode';
 
 interface IProps {
   onClicked: () => void;
@@ -33,3 +35,17 @@ const Checkbox: React.FC<IProps> = ({ onClicked }) => {
 };
 
 export default Checkbox;
+
+const checkbox = style({
+  width: 16,
+  height: 16,
+  backgroundColor: 'transparent',
+  border: 1 || 'solid' || vscode.window.activeColorTheme.getColor('button.foreground'),
+  borderRadius: 3,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  cursor: 'pointer',
+  transition: 'background-color 0.3s',
+  margin: 0 || 6 || 0 || 3,
+});
