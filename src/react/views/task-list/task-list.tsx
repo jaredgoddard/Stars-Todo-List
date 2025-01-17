1bcc42d5fd5015c5a6b4ce132ed56754e022a292
roomee1-admin <jaredbgoddard@gmail.com>
2025-01-16 21:40:09 -0800

    ux improvements

--- src/react/views/task-list/task-list.tsx
+++ src/react/views/task-list/task-list.tsx
@@ -9,6 +9,7 @@ const TaskList = () => {
   const [taskList, _] = useAtom(taskListAtom);
   
   const handleTaskSubmit = (value: string) => { 
+    if(value === '') return;
     createTask(value);
   };
   
@@ -18,6 +19,7 @@ const TaskList = () => {
   
   const isLoading = useMemo(() => taskList === undefined, [taskList]);
   const noFolder = useMemo(() => taskList === null, [taskList]);
+  const noTasks = useMemo(() => taskList && taskList.length === 0, [taskList]);
   const taskComponents = useMemo(() => {
     if(taskList === undefined || taskList === null) return <></>;
     return taskList.map((task, index) => (
@@ -34,6 +36,7 @@ const TaskList = () => {
       {isLoading && <p className={styles.text}>Loading...</p>}
       {noFolder && <p className={styles.text}>No Folder is Open.</p>}
       {!isLoading && !noFolder && <AddTaskTextField onSubmit={handleTaskSubmit}/>}
+      {noTasks && <p className={styles.text}>↑ Add a Task ↑</p>}
       {taskComponents}
     </div>
   );
